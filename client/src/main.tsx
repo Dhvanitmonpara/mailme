7bf95f5516da38ddf3de53bcfcc14f9ac5eb9c43
Dhvanit Monpara <143581064+Dhvanitmonpara@users.noreply.github.com>
2024-09-17 23:13:40 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
 import App from "./App.tsx";
 import "./index.css";
-
+ 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <NextUIProvider>
